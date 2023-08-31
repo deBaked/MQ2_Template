9ada6304ce393ccdb14ad3c1433dd1816ac83975
Daisy Baker <daibak08@gmail.com>
2023-08-31 22:05:13 +0100

    Added interactable objects and free prop pack for example interactable objects
    These objects can be grabbed and picked up by the player, following hand pos

--- Assets/Scripts/ActivateTeleportRay.cs
+++ Assets/Scripts/ActivateTeleportRay.cs
@@ -6,10 +6,11 @@ using UnityEngine.InputSystem;
 public class ActivateTeleportRay : MonoBehaviour
 {
     [SerializeField] GameObject RTele;
-    [SerializeField] InputActionProperty rActivate;
+    [SerializeField] InputActionProperty rActivate, rCancel;
 
     void Update()
     {
-        RTele.SetActive(rActivate.action.ReadValue<float>() > 0.1f);
+        // only enable teleporting if trigger is being pressed AND player is not holding an item 
+        RTele.SetActive(rCancel.action.ReadValue<float>() == 0 && rActivate.action.ReadValue<float>() > 0.1f);
     }
 }
