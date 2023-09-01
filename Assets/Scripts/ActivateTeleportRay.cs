45681606f3a0bc82dd78442e619a61562d3106fa
Daisy Baker <daibak08@gmail.com>
2023-09-01 15:39:18 +0100

    Added function UI for VR
    added support for players to interact with UI elements. Also added feature to avoid bug which would let you teleport whilst interacting with UI.

--- Assets/Scripts/ActivateTeleportRay.cs
+++ Assets/Scripts/ActivateTeleportRay.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportRay : MonoBehaviour
 {
     [SerializeField] GameObject RTele;
     [SerializeField] InputActionProperty rActivate, rCancel;
 
+    [SerializeField] XRRayInteractor rRay;
+
     void Update()
     {
-        // only enable teleporting if trigger is being pressed AND player is not holding an item 
-        RTele.SetActive(rCancel.action.ReadValue<float>() == 0 && rActivate.action.ReadValue<float>() > 0.1f);
+        bool isRRayHovering = rRay.TryGetHitInfo(out Vector3 rPos, out Vector3 rNormal, out int rNumber, out bool rValid);
+
+        // only enable teleporting if not hovering with ray AND trigger is being pressed AND player is not holding an item 
+        RTele.SetActive(!isRRayHovering && rCancel.action.ReadValue<float>() == 0 && rActivate.action.ReadValue<float>() > 0.1f);
     }
 }
