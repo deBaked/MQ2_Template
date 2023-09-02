1ca34d6c6f951c79827f13d062ccc0586ba84ec6
Daisy Baker <daibak08@gmail.com>
2023-09-02 18:04:55 +0100

    Game menu now spawns infront of player with dropdown to choose turn style
    player can choose between continuous turn or snap turn style

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject menu, playerHead;
+    [SerializeField] InputActionProperty showButton;
+    [SerializeField] float spawnDistance = 2f;
+
+    void Update()
+    {
+        if (showButton.action.WasPerformedThisFrame())
+        {
+            menu.transform.position = new Vector3(playerHead.transform.forward.x, 0, playerHead.transform.forward.z).normalized * spawnDistance;
+            menu.SetActive(!menu.activeSelf);
+        }
+
+        if (menu.activeSelf)
+        {
+            // menu tracks player head position and looks toward it
+            menu.transform.LookAt(new Vector3(playerHead.transform.position.x, playerHead.transform.position.y, playerHead.transform.position.z));
+            menu.transform.forward *= -1; // this flips the forward direction so menu will always face player
+        }
+    }
+}
